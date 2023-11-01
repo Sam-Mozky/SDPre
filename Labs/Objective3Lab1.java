2f03d62d2ab3dfdfab5b3b764bc846d28af7acc1
Samantha <someartbysam@gmail.com>
2023-11-01 00:30:28 -0600

    CompleteObjective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1, num2, num3, average;
+		num1 = 12;
+		num2 = 23;
+		num3 = 34;
+		average = (num1 + num2 + num3)/3;
+		System.out.println(average);
+	}
+}
