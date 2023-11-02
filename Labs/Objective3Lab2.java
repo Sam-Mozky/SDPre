1800bcd0214bb9824e325a09b51ca653899fa8b0
Samantha <someartbysam@gmail.com>
2023-11-01 23:41:40 -0600

    CompleteObjective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1, side2, hypotenuse, result;
+		side1 = 12;
+		side2 = 18;
+		hypotenuse = (side1 * side1) + (side2 * side2);
+		result = Math.sqrt(hypotenuse);
+		System.out.println(result);
+	}
+}
