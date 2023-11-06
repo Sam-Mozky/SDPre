403995567b1cc986e9af920ca318051321cea800
Samantha <someartbysam@gmail.com>
2023-11-06 00:20:52 -0700

    CompleteObjective5Lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,17 @@
+public class Objective5Lab3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int userNum;
+
+		System.out.println("Please enter a number");
+		int intInput = keyboard.nextInt();
+
+		if(userNum >= 0) {
+			System.out.println("The number is positive");
+		}
+		else; {
+			System.out.println("The number is negative");
+		}
+		scanner.close();
+	}
+}
