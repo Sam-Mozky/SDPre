076bdfc28e4e56b0bf0fa97619cb05d59dc10801
Samantha <someartbysam@gmail.com>
2023-11-05 23:03:25 -0700

    CompleteObjective5Lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,20 @@
+public class Objective5Lab1 {
+	public static void main(String[] args) {
+		boolean bool1, bool2;
+		int num1 = 7;
+		int num2 = 21;
+
+		bool1 = (num1 > num2);
+		bool2 = (num1 < num2);
+
+		if (num1 > num2) {
+		System.out.println("7 is greater than 21");
+
+		}
+		else if(num2 > num1) {
+		System.out.println("21 is greater than 7");
+
+		}
+
+	}
+}
