797435da01065a2851c23e0dd68ea3feabd94be0
Samantha <someartbysam@gmail.com>
2023-11-10 11:49:59 -0700

    CompleteObjective7Lab5

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,22 @@
+public class Objective7Lab5 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int selection;
+		
+
+		while (true) {
+			System.out.println("_____Menu_____");
+			System.out.println("1: Say Hello");
+			System.out.println("2: List my favorite foods");
+			System.out.println("3: Exit");
+			System.out.println();
+
+			selection = scanner.nextInt();
+
+		if 
+
+		scanner.close();
+	}
+}
+
+}
