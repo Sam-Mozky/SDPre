4d43b1b5b19567eb7dd2ae163423d4cf5df1e4ab
Samantha <someartbysam@gmail.com>
2023-11-09 22:44:01 -0700

    CompleteObjective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 0;
+		int sum = count + 1;
+
+		while(count <= 20) {
+			System.out.println(sum);
+		
+		count = count + 1;
+
+		sum = count + sum;
+
+		}
+	}
+}
