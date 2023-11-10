43391ca4e18c02812215a448d8290b0b37d8bfd0
Samantha <someartbysam@gmail.com>
2023-11-10 01:40:21 -0700

    CompleteObjective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+
+		for(int a = 1; a < 11; a += 1) {
+			System.out.println( a );
+		}
+		for(int b = 10; b > 0; b -=1) {
+			System.out.println( b );
+		}
+	}
+}
